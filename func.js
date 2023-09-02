17e12bf3622def826f0609a52af7d43bf1d0ab38
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2023-09-02 12:12:26 +0530

    added sum function

--- func.js
+++ func.js
@@ -1,3 +1,15 @@
+//declaring a function
 function sayHello(){
     console.log("hello world");
 }
+
+//invoking a function or calling
+sayHello();
+
+//write a function that took two number as parameter and display their sum
+function sum(num1,num2){
+    console.log(num1+num2);
+}
+
+//invoke sum function
+sum();
